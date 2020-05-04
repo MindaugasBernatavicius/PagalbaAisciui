f97f16b85af4acb9d92eda35f5c74fe004e1231f
MindaugasBernatavicius <darbas.mindaugas@gmail.com>
2020-05-04 18:06:31 +0300

    Finishing 4th task

--- _4_pamoka.hpp
+++ _4_pamoka.hpp
@@ -4,40 +4,93 @@
 int rasosMasyvas [] = {37, 49, 58, 125, 200, 545, 685, 721};
 int rimoMasyvas [] = {49, 52, 66, 133, 200};
 
-void ketvirtaPamoka(){
-
-    // ... suskaiciuojame kiek rasos masyve yra dvizenkliu
+int kiekDvizenkliu(int arr [], int masyvoIlgis){
     int kiekDvizenkliu = 0;
-    for(int i = 0; i < sizeof(rasosMasyvas)/sizeof(rasosMasyvas[0]); i++){
-        if(rasosMasyvas[i] > 99){
+    for(int i = 0; i < masyvoIlgis; i++){
+        if(arr[i] > 99){
             kiekDvizenkliu = i;
             break;
         }
     }
+    return kiekDvizenkliu;
+}
+
+int kiekTrizenkliu(int arr [], int masyvoIlgis){
+    int kiekTrizenkliu = 0;
+    for(int i = 0; i < masyvoIlgis; i++){
+        if(arr[i] > 99){
+            kiekTrizenkliu = masyvoIlgis - i;
+            break;
+        }
+    }
+    return kiekTrizenkliu;
+}
+
+void ketvirtaPamoka(){
+    int rasosMasyvoDydis = sizeof(rasosMasyvas) / sizeof(rasosMasyvas[0]);
+    int rimoMasyvoDydis = sizeof(rimoMasyvas) / sizeof(rimoMasyvas[0]);
+
+    // ... suskaiciuojame kiek rasos masyve yra dvizenkliu
+    int dviezenkliuSkaiciusRasosMasyve = kiekDvizenkliu(rasosMasyvas, rasosMasyvoDydis);
+    int dviezenkliuSkaiciusRimoMasyve = kiekDvizenkliu(rimoMasyvas, rimoMasyvoDydis);
 
     // ... kuriame nauja, didesni masyva rimui, kuris talpins senus rimo indeksus ir naujus dvizenklius
-    int naujoRimoMasyvoDydis = (sizeof(rimoMasyvas)/sizeof(rimoMasyvas[0])) + kiekDvizenkliu;
-    int naujasRimoMasyvas[naujoRimoMasyvoDydis];
+    int naujoRasosMasyvoDydis = dviezenkliuSkaiciusRasosMasyve + dviezenkliuSkaiciusRasosMasyve;
+    int naujasRasosMasyvas[naujoRasosMasyvoDydis];
 
     // ... konstruojame nauja rimo masyva
-    // TODO :: nebedeti 3-zenkliu skaiciu !!!!
     int kiekRasosPanaudota = 0;
     int kiekRimoPanaudota = 0;
-    for(int i = 0; i < naujoRimoMasyvoDydis; i++){
-        if(kiekRasosPanaudota < kiekDvizenkliu){
-            if(rimoMasyvas[kiekRimoPanaudota] < rasosMasyvas[kiekRasosPanaudota]){
-                naujasRimoMasyvas[i] = rimoMasyvas[kiekRimoPanaudota];
-                kiekRimoPanaudota++;
-            } else {
-                naujasRimoMasyvas[i] = rasosMasyvas[kiekRasosPanaudota];
-                kiekRasosPanaudota++;
-            }
-        } else {
-            naujasRimoMasyvas[i] = rimoMasyvas[kiekRimoPanaudota];
+    for(int i = 0; i < naujoRasosMasyvoDydis; i++){
+        if(rimoMasyvas[kiekRimoPanaudota] < rasosMasyvas[kiekRasosPanaudota]){
+            naujasRasosMasyvas[i] = rimoMasyvas[kiekRimoPanaudota];
             kiekRimoPanaudota++;
+        } else {
+            naujasRasosMasyvas[i] = rasosMasyvas[kiekRasosPanaudota];
+            kiekRasosPanaudota++;
         }
     }
 
+    cout << "Rasos kolekcija:" << endl;
+    for(int i = 0; i < naujoRasosMasyvoDydis; i++){
+        cout << naujasRasosMasyvas[i];
+        if(i != naujoRasosMasyvoDydis - 1)
+            cout << ", ";
+    }
+    cout << endl;
+
+
+    // ... suzinome, kiek trizenkliu skaiciu yra masyvuose
+    int trizenkliuSkaiciusRasosMasyve = kiekTrizenkliu(rasosMasyvas, rasosMasyvoDydis);
+    int trizenkliuSkaiciusRimoMasyve = kiekTrizenkliu(rimoMasyvas, rimoMasyvoDydis);
+
+    int trizenkliuPradziaRasosMasyve = rasosMasyvoDydis - trizenkliuSkaiciusRasosMasyve;
+    int trizenkliuPradziaRimoMasyve = rimoMasyvoDydis - trizenkliuSkaiciusRimoMasyve;
+
+    int naujoRimoMasyvoDydis = trizenkliuSkaiciusRasosMasyve + trizenkliuSkaiciusRimoMasyve;
+    int naujasRimoMasyvas[naujoRimoMasyvoDydis];
+
+
+    kiekRasosPanaudota = 0;
+    kiekRimoPanaudota = 0;
+    for(int i = 0; i < naujoRimoMasyvoDydis; i++){
+        if(trizenkliuPradziaRimoMasyve + kiekRimoPanaudota < rimoMasyvoDydis){
+            if(rimoMasyvas[trizenkliuPradziaRimoMasyve + kiekRimoPanaudota]
+                < rasosMasyvas[trizenkliuPradziaRasosMasyve + kiekRasosPanaudota])
+            {
+                naujasRimoMasyvas[i] = rimoMasyvas[trizenkliuPradziaRimoMasyve + kiekRimoPanaudota];
+                kiekRimoPanaudota++;
+            } else {
+                naujasRimoMasyvas[i] = rasosMasyvas[trizenkliuPradziaRasosMasyve + kiekRasosPanaudota];
+                kiekRasosPanaudota++;
+            }
+        } else {
+            naujasRimoMasyvas[i] = rasosMasyvas[trizenkliuPradziaRasosMasyve + kiekRasosPanaudota];
+            kiekRasosPanaudota++;
+        }
+    }
+
+    cout << "Rimo kolekcija:" << endl;
     for(int i = 0; i < naujoRimoMasyvoDydis; i++){
         cout << naujasRimoMasyvas[i];
         if(i != naujoRimoMasyvoDydis - 1)
@@ -45,26 +98,11 @@ void ketvirtaPamoka(){
     }
     cout << endl;
 
+//    cout << "trizenkliuSkaiciusRasosMasyve: " << trizenkliuSkaiciusRasosMasyve << endl;
+//    cout << "trizenkliuSkaiciusRimoMasyve: " << trizenkliuSkaiciusRimoMasyve << endl;
+//
+//    cout << "trizenkliuPradziaRasosMasyve: " << trizenkliuPradziaRasosMasyve << endl;
+//    cout << "trizenkliuPradziaRimoMasyve: " << trizenkliuPradziaRimoMasyve << endl;
 
-    // ... suzinome, kiek trizenkliu skaiciu yra rimo masyve ir nuo kurios vietos ji prasideda
-    int kiekTrizenkliu = 0;
-    int trizenkliuPradzia = 0;
-    int senoRimoMasyvoDydis = sizeof(rimoMasyvas)/sizeof(rimoMasyvas[0]);
-    for(int i = 0; i < senoRimoMasyvoDydis; i++){
-        if(rimoMasyvas[i] > 99){
-            kiekTrizenkliu = senoRimoMasyvoDydis - i;
-            trizenkliuPradzia = i;
-            break;
-        }
-    }
-
-
-    // ... konstruojame naujaji rasos masyva - iskart nebedesime dvizeznkliu skaiciu
-    for(int i = trizenkliuPradzia; i < kiekTrizenkliu + trizenkliuPradzia; i++){
-        cout << rimoMasyvas[i] << endl;
-    }
-
-    cout << endl << "kiekTrizenkliu : " << kiekTrizenkliu << endl;
-    cout << endl << "trizenkliuPradzia : " << trizenkliuPradzia << endl;
     exit (EXIT_SUCCESS);
 }
